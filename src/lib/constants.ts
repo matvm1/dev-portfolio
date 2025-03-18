83a92284cebc720c7d4a46d20f4746a2e977452f
Mateo Vergara <15144366+matvm1@users.noreply.github.com>
2025-03-17 22:31:12 -0400

    Personalized schema information in layouts. Personalized links in src/lib/constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,8 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://dribbble.com/Ladvace_Jace",
-  email: "contact@gianmarcocavallo.com<",
+  github: "https://github.com/matvm1",
+  linkedin: "https://www.linkedin.com/in/mateo-vergara/",
+  discord: "https://discordapp.com/users/393846237255696385",
+  email: "contact@mateodev.com<",
 };
 
 export const loaderAnimation = [

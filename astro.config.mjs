d0c6d4153e1b94f2a3a0b38c95f86711ed29feb4
Mateo <--global>
2025-03-18 20:52:05 -0400

    Updated README with acknowledgements. Updated site links

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,13 +12,13 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarcocavallo.com/",
+  site: "https://mateodev.com/",
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: [
-        "https://gianmarcocavallo.com/sitemap-index.xml",
-        "https://gianmarcocavallo.com/sitemap-0.xml",
+        "https://mateodev.com/sitemap-index.xml",
+        "https://mateodev.com/sitemap-0.xml",
       ],
     }),
     solidJs(),
